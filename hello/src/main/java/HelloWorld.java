e679ab595311a0748a1db79e7b3431af0af4fb45
srikanth <srikanth.naidu65@gmail.com>
2023-01-01 01:47:20 +0530

    Added assignment1 files

---
+++ hello/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author  Srikanth
+ * @since   01/01/2023
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
---
+++ hello/src/main/java/RandomWord.java
@@ -0,0 +1,23 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+/**
+ * @author  Srikanth
+ * @since   01/01/2023
+ */
+public class RandomWord {
+
+    public static void main(String[] args) {
+
+        String champion = "";
+
+        for (double i=1; !StdIn.isEmpty(); i++) {
+            String input = StdIn.readString();
+            if (StdRandom.bernoulli(1 / i)) {
+                champion = input;
+            }
+        }
+        StdOut.println(champion);
+    }
+}
